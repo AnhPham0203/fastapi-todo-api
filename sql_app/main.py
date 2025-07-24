7cba7f461592fe935b9b81885821616273f05051
Anh Pham <anh.pham.ngoc@madison.dev>
2025-07-24 09:32:37 +0700

    Fix: Update JWT decoding to use settings for secret key and algorithm

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,12 +1,11 @@
 # sql_app/main.py
 
 import jwt
-from jwt import PyJWTError
 from fastapi import FastAPI, Depends, HTTPException  # type: ignore
 from sqlalchemy.orm import Session
 from . import security
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm  # type: ignore
-
+from .config import settings
 # Import các thành phần từ các file khác trong cùng thư mục
 from . import models, schemas
 from .database import SessionLocal, engine
@@ -103,12 +102,12 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, security.SECRET_KEY,
-                             algorithms=[security.ALGORITHM])
+        payload = jwt.decode(token, settings.SECRET_KEY,
+                             algorithms=[settings.ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-    except PyJWTError:
+    except JWTError:  # type: ignore
         raise credentials_exception
 
     user = db.query(models.User).filter(
